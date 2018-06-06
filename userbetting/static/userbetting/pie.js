1f5cdcfd1ee628f91460ab1fed90325c50c676bf
Nexvos <marc.cerretti@gmail.com>
2018-06-06 23:27:15 +0100

    Made the chart updatable \o/ - Should be working now - Need to remove printing to the console and in python still

--- userbetting/static/userbetting/pie.js
+++ userbetting/static/userbetting/pie.js
@@ -1,173 +1,14 @@
-
-
+//svg size variables - also controls size of pie
 var w=700,h=500;
 
-var svg=d3.select("#chart")
-        .append("svg")
-        .attr({
-            width:w,
-            height:h,
-            class:'shadow'
-        }).append('g')
-        .attr({
-            transform:'translate('+w/2+','+h/2+')'
-        });
-
-var outerRadius=Math.min(w, h) / 2;
-var innerRadius=outerRadius - 80;
-
-var arc=d3.svg.arc()
-        .outerRadius(outerRadius)
-        .innerRadius(innerRadius);
-
-
-var pie=d3.layout.pie()
-        .value(function(d){return d.percent})
-        .sort(null)
-        .padAngle(0.02);
-
-
-//tooltip creation
-var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-
-var path=svg.selectAll('path')
-        .data(pie(dataset))
-        .enter()
-        .append('path')
-        .attr({
-            d:arc,
-            fill: function(d,i){
-                return color(d.data.name);
-            }
-        })
-		//tooltip mouseover animation
-		.on("mouseover", mouseover)
-        .on("mouseout", mouseout);
-
-
-
-function mouseover(d) {
-	d3.select(this).transition()
-		.duration(200)
-		.style("stroke", "black")
-		.style("stroke-width", "1");
-	div.transition()
-		.duration(200)
-		.style("opacity", .85)
-		.style("background", color(d.data.name));
-	div.html("Name: " + d.data.name + "<br>" + "£" + d.data.amount.toFixed(2))
-		.style("left", (d3.event.pageX) + "px")
-		.style("top", (d3.event.pageY - 28) + "px");
-	};
-
-function mouseout(d) {
-	d3.select(this).transition()
-		.duration(200)
-		.style("stroke", "none");
-	div.transition()
-		.duration(500)
-		.style("opacity", 0);
-};
-
-path.transition()
-    .duration(800)
-    .attrTween('d', function(d) {
-        var interpolate = d3.interpolate({startAngle: 0, endAngle: 0}, d);
-        return function(t) {
-            return arc(interpolate(t));
-        };
-    });
-
-//percentage text
-var text=svg.selectAll('text')
-  .data(pie(dataset))
-  .enter()
-  .append("text")
-  .transition()
-  .duration(1000)
-  .attr("transform", function (d) {
-      return "translate(" + arc.centroid(d) + ")";
-  })
-  .attr("dy", ".4em")
-  .attr("text-anchor", "middle")
-  .attr("class", "strokeme")
-  .text(function(d){ if (d.data.percent>2){
-  	return d.data.percent.toFixed(2)+"%";
-  }})
-  ;
-
-
-//Creation of the legend
+//legend variables
 var legendRectSize=20;
 var legendSpacing=7;
 var legendHeight=legendRectSize+legendSpacing;
 
+function InitialPie(dataset, totalamount) {
 
-var legend=svg.selectAll('.legend')
-  .data(color.domain())
-  .enter()
-  .append('g')
-  .attr({
-      class:'legend',
-      transform:function(d,i){
-          //Just a calculation for x and y position
-          return 'translate('+ (outerRadius + 50) + ',' + ((i*legendHeight) - h/2 +100) + ')';
-      }
-  });
-
-
-legend.append('rect')
-  .attr({
-      width:legendRectSize,
-      height:legendRectSize,
-      rx:20,
-      ry:20
-  })
-  .style({
-      fill:color,
-      stroke:color
-  });
-
-legend.append('text')
-  .attr({
-      x:30,
-      y:15
-  })
-  .text(function(d){
-      return d;
-  });
-
-//total
-var totalLabel = svg.append("text")
-	.html("Total money bet:")
-	.attr({
-		  class:'total',
-		  transform:function(d,i){
-			  //Just a calculation for x and y position
-			  return 'translate(-70,' + -15 + ')';
-		  }
-	  });
-var total = svg.append("text")
-	.html("£" + totalamount.toFixed(2))
-	.attr({
-		  class:'total',
-		  transform:function(d,i){
-			  //Just a calculation for x and y position
-			  return 'translate(-35,' + 15 + ')';
-		  }
-	  });
-
-
-//button
-function change(asd) {
-	var rawData = asd;
-
-
-	var w=700,h=500;
-
+	//Create the SVG with correct attributes and transformed g element
 	var svg=d3.select("#chart")
 			.append("svg")
 			.attr({
@@ -179,28 +20,24 @@ function change(asd) {
 				transform:'translate('+w/2+','+h/2+')'
 			});
 
+	//Set outer and inner radius of the donut
 	var outerRadius=Math.min(w, h) / 2;
 	var innerRadius=outerRadius - 80;
 
+	//Create d3 arc - This sets up the creation of the
 	var arc=d3.svg.arc()
 			.outerRadius(outerRadius)
 			.innerRadius(innerRadius);
 
-
+	//Create pie
 	var pie=d3.layout.pie()
 			.value(function(d){return d.percent})
 			.sort(null)
 			.padAngle(0.02);
 
-
-	//tooltip creation
-	var div = d3.select("body").append("div")
-		.attr("class", "tooltip")
-		.style("opacity", 0);
-
-
+	// Creates the pie elements
 	var path=svg.selectAll('path')
-			.data(pie(rawData))
+			.data(pie(dataset))
 			.enter()
 			.append('path')
 			.attr({
@@ -214,6 +51,10 @@ function change(asd) {
 			.on("mouseout", mouseout);
 
 
+	//tooltip creation
+	var div = d3.select("body").append("div")
+		.attr("class", "tooltip")
+		.style("opacity", 0);
 
 	function mouseover(d) {
 		d3.select(this).transition()
@@ -238,6 +79,7 @@ function change(asd) {
 			.style("opacity", 0);
 	};
 
+	// Animation of the pie elements
 	path.transition()
 		.duration(800)
 		.attrTween('d', function(d) {
@@ -249,7 +91,7 @@ function change(asd) {
 
 	//percentage text
 	var text=svg.selectAll('text')
-	  .data(pie(rawData))
+	  .data(pie(dataset))
 	  .enter()
 	  .append("text")
 	  .transition()
@@ -260,6 +102,7 @@ function change(asd) {
 	  .attr("dy", ".4em")
 	  .attr("text-anchor", "middle")
 	  .attr("class", "strokeme")
+	  .attr("id", "percentagetext")
 	  .text(function(d){ if (d.data.percent>2){
 		return d.data.percent.toFixed(2)+"%";
 	  }})
@@ -267,11 +110,6 @@ function change(asd) {
 
 
 	//Creation of the legend
-	var legendRectSize=20;
-	var legendSpacing=7;
-	var legendHeight=legendRectSize+legendSpacing;
-
-
 	var legend=svg.selectAll('.legend')
 	  .data(color.domain())
 	  .enter()
@@ -317,173 +155,165 @@ function change(asd) {
 			  }
 		  });
 	var total = svg.append("text")
-		.html("£" + total_bet.toFixed(2))
+		.html("£" + totalamount.toFixed(2))
 		.attr({
 			  class:'total',
 			  transform:function(d,i){
 				  //Just a calculation for x and y position
 				  return 'translate(-35,' + 15 + ')';
 			  }
-		  });
+		  })
+};
 
 
-  }
+//button
+function change(asd, ads) {
+
+    var svg = d3.select("#chart")
+		.select("svg")
+		.select("g");
+
+    // var oldData = svg.selectAll("path")
+    //  .data().map(function(d) { return d.data });
+    // console.log(oldData);
+
+    var path = svg.selectAll('path')
+
+	var pie=d3.layout.pie()
+			.value(function(d){return d.percent})
+			.sort(null)
+			.padAngle(0.02);
+    //tooltip creation
+
+	var outerRadius=Math.min(w, h) / 2;
+	var innerRadius=outerRadius - 80;
+
+	var arc=d3.svg.arc()
+			.outerRadius(outerRadius)
+			.innerRadius(innerRadius);
+
+    path.each(function (d) {
+        this._current = d;
+    }); // store the initial angles
+
+
+    path.data(pie(asd));
+    path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+    console.log(asd);
+
+    // path.data(pie(oldData))
+    // 	.exit();
+
+    $("#fadein").attr("id", "");
 
 
 
-// d3.select("#update")
-// 	.on("click", function() {
-// 		change();
-// 	})
+    path.data(pie(asd))
+        .enter()
+        .append('path')
+        .attr({
+            d: arc,
+            fill: function (d, i) {
+                return color(d.data.name);
+            },
+            "id": "fadein"
+        })
+        //tooltip mouseover animation
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
+
+    var div = d3.select("body").append("div")
+		.attr("class", "tooltip")
+		.style("opacity", 0);
+
+	function mouseover(d) {
+		d3.select(this).transition()
+			.duration(200)
+			.style("stroke", "black")
+			.style("stroke-width", "1");
+		div.transition()
+			.duration(200)
+			.style("opacity", .85)
+			.style("background", color(d.data.name));
+		div.html("Name: " + d.data.name + "<br>" + "£" + d.data.amount.toFixed(2))
+			.style("left", (d3.event.pageX) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
+		};
+
+	function mouseout(d) {
+		d3.select(this).transition()
+			.duration(200)
+			.style("stroke", "none");
+		div.transition()
+			.duration(500)
+			.style("opacity", 0);
+	};
+
+    path.transition().duration(750).attrTween("d", arcTween);
 
 
+    $("#fadein").hide().fadeIn(3000);
 
 
+    function arcTween(a) {
+        var i = d3.interpolate(this._current, a);
+        this._current = i(0);
+        return function (t) {
+            return arc(i(t));
+        };
+    }
+
+    //Percentage Text
+	var text=svg.selectAll('#percentagetext')
+	    .data(pie(asd))
+	 	.enter()
+		.append("text")
+		.attr("id", "percentagetext");
+	svg.selectAll('#percentagetext').transition()
+	  .duration(1000)
+	  .attr("transform", function (d) {
+		  return "translate(" + arc.centroid(d) + ")";
+	  })
+	  .attr("dy", ".4em")
+	  .attr("text-anchor", "middle")
+	  .attr("class", "strokeme")
+	  .text(function(d){ if (d.data.percent>2){
+		return d.data.percent.toFixed(2)+"%";
+	  }})
+	  ;
+		//Creation of the legend
+	var legend=svg.selectAll('.legend')
+	  .data(color.domain())
+	  .enter()
+	  .append('g')
+	  .attr({
+		  class:'legend',
+		  transform:function(d,i){
+			  //Just a calculation for x and y position
+			  return 'translate('+ (outerRadius + 50) + ',' + ((i*legendHeight) - h/2 +100) + ')';
+		  }
+	  });
 
 
+	legend.append('rect')
+	  .attr({
+		  width:legendRectSize,
+		  height:legendRectSize,
+		  rx:20,
+		  ry:20
+	  })
+	  .style({
+		  fill:color,
+		  stroke:color
+	  });
 
+	legend.append('text')
+	  .attr({
+		  x:30,
+		  y:15
+	  })
+	  .text(function(d){
+		  return d;
+	  });
+};
 
-// var svg = d3.select("body")
-// 	.append("svg")
-// 	.append("g")
-//
-// svg.append("g")
-// 	.attr("class", "slices");
-// svg.append("g")
-// 	.attr("class", "labels");
-// svg.append("g")
-// 	.attr("class", "lines");
-//
-// var width = 960,
-//     height = 450,
-// 	radius = Math.min(width, height) / 2;
-//
-// var pie = d3.layout.pie()
-// 	.sort(null)
-// 	.value(function(d) {
-// 		return d.value;
-// 	});
-//
-// var arc = d3.svg.arc()
-// 	.outerRadius(radius * 0.8)
-// 	.innerRadius(radius * 0.4);
-//
-// var outerArc = d3.svg.arc()
-// 	.innerRadius(radius * 0.9)
-// 	.outerRadius(radius * 0.9);
-//
-// svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-//
-// var key = function(d){ return d.data.label; };
-//
-// var color = d3.scale.ordinal()
-// 	.domain(["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
-// 	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-//
-// // var asdfh = [4,5,6,7,1,7,8,3]
-//
-// function randomData (){
-// 	var labels = color.domain();
-// 	return labels.map(function(label){
-// 		return { label: label, value: asdfh[labels.indexOf(label)]}
-// 	});
-// }
-//
-// change(randomData());
-//
-// d3.select(".updatebutton")
-// 	.on("click", function(){
-// 		change(randomData());
-// 	});
-//
-//
-// function change(data) {
-//
-// 	/* ------- PIE SLICES -------*/
-// 	var slice = svg.select(".slices").selectAll("path.slice")
-// 		.data(pie(data), key);
-//
-// 	slice.enter()
-// 		.insert("path")
-// 		.style("fill", function(d) { return color(d.data.label); })
-// 		.attr("class", "slice");
-//
-// 	slice
-// 		.transition().duration(1000)
-// 		.attrTween("d", function(d) {
-// 			this._current = this._current || d;
-// 			var interpolate = d3.interpolate(this._current, d);
-// 			this._current = interpolate(0);
-// 			return function(t) {
-// 				return arc(interpolate(t));
-// 			};
-// 		})
-//
-// 	slice.exit()
-// 		.remove();
-//
-// 	/* ------- TEXT LABELS -------*/
-//
-// 	var text = svg.select(".labels").selectAll("text")
-// 		.data(pie(data), key);
-//
-// 	text.enter()
-// 		.append("text")
-// 		.attr("dy", ".35em")
-// 		.text(function(d) {
-// 			return d.data.label;
-// 		});
-//
-// 	function midAngle(d){
-// 		return d.startAngle + (d.endAngle - d.startAngle)/2;
-// 	}
-//
-// 	text.transition().duration(1000)
-// 		.attrTween("transform", function(d) {
-// 			this._current = this._current || d;
-// 			var interpolate = d3.interpolate(this._current, d);
-// 			this._current = interpolate(0);
-// 			return function(t) {
-// 				var d2 = interpolate(t);
-// 				var pos = outerArc.centroid(d2);
-// 				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-// 				return "translate("+ pos +")";
-// 			};
-// 		})
-// 		.styleTween("text-anchor", function(d){
-// 			this._current = this._current || d;
-// 			var interpolate = d3.interpolate(this._current, d);
-// 			this._current = interpolate(0);
-// 			return function(t) {
-// 				var d2 = interpolate(t);
-// 				return midAngle(d2) < Math.PI ? "start":"end";
-// 			};
-// 		});
-//
-// 	text.exit()
-// 		.remove();
-//
-// 	/* ------- SLICE TO TEXT POLYLINES -------*/
-//
-// 	var polyline = svg.select(".lines").selectAll("polyline")
-// 		.data(pie(data), key);
-//
-// 	polyline.enter()
-// 		.append("polyline");
-//
-// 	polyline.transition().duration(1000)
-// 		.attrTween("points", function(d){
-// 			this._current = this._current || d;
-// 			var interpolate = d3.interpolate(this._current, d);
-// 			this._current = interpolate(0);
-// 			return function(t) {
-// 				var d2 = interpolate(t);
-// 				var pos = outerArc.centroid(d2);
-// 				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-// 				return [arc.centroid(d2), outerArc.centroid(d2), pos];
-// 			};
-// 		});
-//
-// 	polyline.exit()
-// 		.remove();
-// };
